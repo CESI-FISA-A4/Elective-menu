3709d77043102fcf422a309d548f7824ca0ca12f
Asarck <85520089+Asarck@users.noreply.github.com>
2024-04-09 21:55:37 +0200

    /ping

--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -1,16 +1,17 @@
-const { getProductById, getAllProduct, createProduct, deleteProduct, updateProduct, getProductByRestaurantId, getProductByName } = require('../views/productView')
+const { getProductById, getAllProduct, createProduct, deleteProduct, updateProduct, getProductByRestaurantId, getProductByName, ping } = require('../views/productView')
 
 const { schemaGetAllProduct, schemaGetProductById, schemaGetProductByRestaurantId, schemaGetProductByName, schemaCreateMenu, schemaCreateProduct, schemaUpdateProduct, schemaDeleteProduct } = require('../utils/swagger.schema');
 
-const productRoutes = function(instance, opts, next) {
+const productRoutes = function (instance, opts, next) {
+  instance.get('/ping', ping)
   instance.get('/', schemaGetAllProduct, getAllProduct);
   instance.get('/:productId', schemaGetProductById, getProductById);
   instance.get('/restaurant/:restaurantId', schemaGetProductByRestaurantId, getProductByRestaurantId);
   instance.get('/name/:productName', schemaGetProductByName, getProductByName);
   instance.post('/', schemaCreateProduct, createProduct);
   instance.delete('/:productId', schemaDeleteProduct, deleteProduct);
-  instance.put('/:productId', schemaUpdateProduct,updateProduct);
-    next();
+  instance.put('/:productId', schemaUpdateProduct, updateProduct);
+  next();
 };
 
 
